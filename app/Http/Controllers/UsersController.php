ff7cf818df1938d162e887707c9947d0b55ac27c
chinakaiche <2232041670@qq.com>
2018-05-30 22:44:56 +0800

    注册

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,4 +18,21 @@ class UsersController extends Controller
 
         return view('users.show',compact('user'));
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6|max:16'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success','欢迎开启一段新的旅程~~');
+        return redirect()->route('users.show',[$user]);
+    }
 }
